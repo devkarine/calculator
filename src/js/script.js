c0383d70f05fefe6de7a0f76458e6e6f03c3b212
Karine Pereira <karinedeveloper@gmail.com>
2023-06-18 14:32:17 -0300

    refatoracao

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,24 @@
 const del = document.getElementById("del");
 const reset = document.getElementById("reset");
 const res = document.getElementById("res");
-
 const two = document.getElementById("t2");
-two.addEventListener("click", function () {
+const three = document.getElementById("t3");
+const one = document.getElementById("t1");
+
+two.addEventListener("click", () => {
   const body = document.querySelector("body");
   body.classList.add("theme02");
   body.classList.remove("theme03");
   body.classList.remove("theme01");
 });
 
-const three = document.getElementById("t3");
-three.addEventListener("click", function () {
+three.addEventListener("click", () => {
   const body = document.querySelector("body");
   body.classList.add("theme03");
   body.classList.remove("theme02");
 });
 
-const one = document.getElementById("t1");
-one.addEventListener("click", function () {
+one.addEventListener("click", () => {
   const body = document.querySelector("body");
   body.classList.remove("theme02");
   body.classList.remove("theme03");
